6a67e963af5345b7112809b497609f9d78c84d86
Kishore Varma <kishore.nadimpalli@orionhealth.com>
2017-08-14 15:15:17 +1200

    added proptypes needs refractoring later

--- src/components/component-common.js
+++ src/components/component-common.js
@@ -2,25 +2,47 @@ import React, { PropTypes } from 'react';
 import { styleClassNames } from './prop-types';
 
 const ComponentCommon = () => {
-    return (
-        <h1>This component just holds props and default props.</h1>
-    );
-}
+  return <h1>This component just holds props and default props.</h1>;
+};
 
 ComponentCommon.propTypes = {
-    ...styleClassNames,
-    errorMessages: PropTypes.array,
-    help: PropTypes.string,
-    label: PropTypes.string,
-    layout: PropTypes.oneOf(['horizontal', 'vertical', 'elementOnly']),
-    showErrors: PropTypes.bool,
-    onChange: PropTypes.func,
-    onSetValue: PropTypes.func
-}
+  ...styleClassNames,
+  componentRef: PropTypes.any,
+  errorMessages: PropTypes.array,
+  getErrorMessage: PropTypes.any,
+  getErrorMessages: PropTypes.any,
+  getValue: PropTypes.any,
+  hasValue: PropTypes.any,
+  help: PropTypes.string,
+  innerRef: PropTypes.any,
+  isFormDisabled: PropTypes.any,
+  isFormSubmitted: PropTypes.any,
+  isPristine: PropTypes.any,
+  isRequired: PropTypes.any,
+  isValid: PropTypes.any,
+  isValidValue: PropTypes.any,
+  label: PropTypes.string,
+  layout: PropTypes.oneOf(['horizontal', 'vertical', 'elementOnly']),
+  resetValue: PropTypes.any,
+  setValidations: PropTypes.any,
+  setValue: PropTypes.any,
+  showError: PropTypes.any,
+  showErrors: PropTypes.bool,
+  showRequired: PropTypes.any,
+
+  // // Formsy props we don't use
+  validationError: PropTypes.any,
+  validationErrors: PropTypes.any,
+  validations: PropTypes.any,
+
+  // // HOC refs
+  onChange: PropTypes.func,
+  onSetValue: PropTypes.func
+};
 
 ComponentCommon.defaultProps = {
-    onSetValue: () => {},
-    onChange: () => {}
-}
+  onSetValue: () => {},
+  onChange: () => {}
+};
 
 export default ComponentCommon;
